c98328f96ee1ea53fb2b08eb49e3443aa3ac1473
Bogdan Ponomarenko <ponomarenko.bogdan@yandex.ru>
2015-04-10 01:01:26 +0300

    complete responsive header

--- proj/js/main.js
+++ proj/js/main.js
@@ -1,5 +1,34 @@
 "use strict";
 
-(function () {
-	console.log('Hello, World!');
+var app = (function () {
+	return {
+        init: function () {
+            this.setUpListeners();
+        },
+        setUpListeners: function () {
+            $('.show').on('click', this.showMenu);
+        },
+        showMenu: function (e) {
+            e.preventDefault();
+
+            var nav = $('.nav'),
+                    expression = parseInt( nav.css('top') );
+
+            console.log(expression);
+
+            if (expression) {
+                nav.animate({
+                    top: 0
+                }, 300);
+            } else {
+                nav.animate({
+                    top: -1*nav.height() + 'px'
+                }, 300)
+            }
+
+
+        }
+    }
 })();
+
+app.init();
